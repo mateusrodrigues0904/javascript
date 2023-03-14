49c6c9af5cb78dd73648a796f914c96385322eb0
mateusrodrigues0904 <125597047+mateusrodrigues0904@users.noreply.github.com>
2023-03-14 01:10:50 -0300

    curso js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel} KM/H`)
+if (vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga sempre usando cinto de segurança.`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = "BRASIL"
+if (pais == "BRASIL"){
+    console.log(`Brasileiro!`)
+}
+else{
+    console.log(`Estrangeiro`)
+}
