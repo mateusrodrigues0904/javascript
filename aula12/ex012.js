0d91b355754d7c46a0597ee208c8d249117b697c
mateusrodrigues0904 <125597047+mateusrodrigues0904@users.noreply.github.com>
2023-03-15 16:12:24 -0300

    ex2

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,13 +1,11 @@
-var agora = new Date()
-var agora = agora.getHours()
-var hora = 22
-console.log(`São exatamente ${hora} horas.`)
-if (hora < 12){
-    console.log(`E está de manhã.`)
-}
-else if(hora > 12 && hora < 19){
-    console.log(`E está de tarde.`)
-}
-else if(hora > 18 && hora <= 23){
-    console.log(`E está de noite.`)
+var agora = new Date();
+var agora = agora.getHours();
+var hora = 22;
+console.log(`São exatamente ${hora} horas.`);
+if (hora < 12) {
+  console.log(`E está de manhã.`);
+} else if (hora > 12 && hora < 19) {
+  console.log(`E está de tarde.`);
+} else if (hora > 18 && hora <= 23) {
+  console.log(`E está de noite.`);
 }
